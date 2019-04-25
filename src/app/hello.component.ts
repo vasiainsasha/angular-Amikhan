a7c7a478437f3d73f25944faba8d654d8fc2fac2
vasiainsasha <180103133@stu.sdu.edu.kz>
2019-04-25 11:19:01 +0600

    1 file modified

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -3,7 +3,7 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'hello',
   template: `<h1>hello Jomart agay!</h1>`,
-  styles: [`h1 { font-family: Lato; }`]
+  styles: [`h1 { font-family: Arial; }`]
 })
 export class HelloComponent  {
   @Input() name: string;
